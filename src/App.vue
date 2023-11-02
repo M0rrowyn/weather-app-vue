ed92aba48484142b58573c8b472a2171c2fe328e
Elena Kryshtal <elenkrishtal@gmail.com>
2023-11-02 13:13:24 +0200

    Create API key and Base url variables, and getWeather fetch method with data from server

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { API_KEY, BASE_URL } from './constants';
+
+const city = ref('Kyiv');
+const weatherInfo = ref(null);
+
+function getWeather() {
+  fetch(`${BASE_URL}?q=${city.value}&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => (weatherInfo.value = data));
+}
+
+onMounted(getWeather);
+</script>
 
 <template>
   <div class="page">
     <div class="weather">
       <div class="city-inner">
-        <input type="text" class="search" />
+        <input
+          v-model="city"
+          class="search"
+          type="text"
+          @keyup.enter="getWeather"
+        />
       </div>
-      <h2 class="city">Weather in:</h2>
+      <h2 class="city">Weather in: {{ city }}</h2>
       <h1 class="temperature">&deg;C</h1>
       <div class="flex">
         <img src="" class="icon" alt="Weather icon" />
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'a91510521b17929808a226f0569f3445';
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
