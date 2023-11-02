972eb4121a1598090dabf6bf0e515948a1f845cd
Elena Kryshtal <elenkrishtal@gmail.com>
2023-11-02 12:30:01 +0200

    Create basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script setup></script>
 
 <template>
-  <h1>Weather App</h1>
+  <div class="page">
+    <div class="weather">
+      <div class="city-inner">
+        <input type="text" class="search" />
+      </div>
+      <h2 class="city">Weather in:</h2>
+      <h1 class="temperature">&deg;C</h1>
+      <div class="flex">
+        <img src="" class="icon" alt="Weather icon" />
+        <div class="description"></div>
+      </div>
+      <div class="pressure">Pressure:</div>
+      <div class="humidity">Humidity:</div>
+      <div class="speed">Wind speed:</div>
+      <div class="deg">Wind direction:</div>
+    </div>
+  </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './main.css';
+import './assets/styles/main.scss';
 
 import { createApp } from 'vue';
 import App from './App.vue';
